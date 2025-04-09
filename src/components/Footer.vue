8364adfe0bb8a48f489a40955dea445b78c38685
Deniz <denzz@denzz.xyz>
2025-04-09 17:58:44 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,13 +4,17 @@ import { ref, onMounted } from 'vue';
 const commit = ref(null);
 
 onMounted(async () => {
-  // commit verisi artık alınmayacak
 })
 </script>
 
 <template>
   <div class="flex justify-between mt-10 gap-5 text-sm text-catppuccin-gray">
-    <!-- GitHub bağlantısını gösteriyoruz -->
-    <a href="https://github.com/d-niz/dniz.tr" target="_blank" class="whitespace-nowrap underline">site repo</a>
+    <a href="https://github.com/d-niz/dniz.tr" target="_blank" class="whitespace-nowrap underline">
+      site repo
+    </a>
+    
+    <span class="whitespace-nowrap">
+      GPG: 0x433B49A88191C661
+    </span>
   </div>
 </template>
