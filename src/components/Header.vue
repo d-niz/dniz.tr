1db6b565b9f993ac079c56929ca9c3f724746afc
Deniz <denzz@denzz.xyz>
2025-04-28 10:35:24 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faDiscord, faInstagram, faMastodon, faTelegram, faSignalMessenger, faMusic } from '@fortawesome/free-brands-svg-icons';
-import { faKey } from '@fortawesome/free-solid-svg-icons';
+import { faDiscord, faInstagram, faMastodon, faTelegram, faSignalMessenger } from '@fortawesome/free-brands-svg-icons';
+import { faKey, faMusic } from '@fortawesome/free-solid-svg-icons'; // <-- faMusic doğru paketten
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 library.add(faDiscord, faInstagram, faMastodon, faTelegram, faSignalMessenger, faKey, faMusic);
@@ -65,7 +66,7 @@ const connectWebSocket = () => {
 };
 
 const fetchLastFmNowPlaying = async () => {
-  const API_KEY = '25456cc62de7291306a1fe391ea550b9'; // 🔧 Last.fm API key'ini buraya yaz
+  const API_KEY = '25456cc62de7291306a1fe391ea550b9';
   const USERNAME = 'den-zz'; 
   const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&api_key=${API_KEY}&format=json&limit=1`;
 
@@ -100,49 +101,3 @@ onUnmounted(() => {
   }
 });
 </script>
-
-<template>
-  <div class="font-sans font-black text-5xl">
-    dniz.tr
-  </div>
-  <div>
-    deniz (aka dniz or denzz), a normal person, arch enjoyer, idk who i am. contact: denzz@denzz.xyz
-  </div>
-
-  <div class="flex gap-2 items-center text-sm mt-2" :class="discordStatusColor">
-    <font-awesome-icon :icon="['fab', 'discord']" class="text-xl w-5 h-5" />
-    <div>
-      i'm currently {{ discordStatus }} on Discord.
-    </div>
-  </div>
-
-  <!-- 🎵 Last.fm Durumu -->
-  <div class="flex gap-2 items-center text-sm mt-2" :class="lastfmColor">
-    <font-awesome-icon icon="music" class="text-xl w-5 h-5" />
-    <div>
-      <span v-if="lastfmTrack">i'm listening {{ lastfmTrack }} right now.</span>
-      <span v-else>i'm not listening anything.</span>
-    </div>
-  </div>
-
-  <div class="flex gap-10 mt-5 text-xl">
-    <a href="https://www.instagram.com/msadenz.z" target="_blank" class="flex items-center justify-center">
-      <font-awesome-icon :icon="['fab', 'instagram']" class="w-5 h-5" />
-    </a>
-    <a href="https://discord.com/user/1259949511078318287" target="_blank" class="flex items-center justify-center">
-      <font-awesome-icon :icon="['fab', 'discord']" class="w-5 h-5" />
-    </a>
-    <a href="https://mastodon.com.tr/@denzz" target="_blank" class="flex items-center justify-center">
-      <font-awesome-icon :icon="['fab', 'mastodon']" class="w-5 h-5" />
-    </a>
-    <a href="https://t.me/use_rname" target="_blank" class="flex items-center justify-center">
-      <font-awesome-icon :icon="['fab', 'telegram']" class="w-5 h-5" />
-    </a>
-    <a href="https://signal.me/#eu/10FRPQwWfQi-MtUfIIBKrjcMynVpyW-UVTrt8KcYdwAOgSPuSwVETbfbJ-WuseF0" target="_blank" class="flex items-center justify-center">
-      <font-awesome-icon :icon="['fab', 'signal-messenger']" class="w-5 h-5" />
-    </a>
-    <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/0FF28A34E3098C31CEAD6034433B49A88191C661" target="_blank" class="flex items-center justify-center">
-      <font-awesome-icon :icon="['fas', 'key']" class="w-5 h-5" />
-    </a>
-  </div>
-</template>
